5a397301083c4db596d3887c7cc99b31ef856e49
Afsar <afsar_zan@yahoo.com>
2024-12-14 22:52:43 -0800

    Counter basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import './App.css'
-import Button from './components/Button'
+import Cart from './components/Cart'
+import Counter from './components/Counter'
 
 function App() {
 
   return (
     <>
-     <h1 className='mb-4'>
-        commit 
-     </h1>
-     <p>Afsar Ahmed</p>
-     <Button text='parent text'/>
+      <Counter />
+      <Cart />
     </>
   )
 }
