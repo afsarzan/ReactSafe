2f43a10822e9a11b631d8bbfd0b7cad8ec56c846
Afsar <afsar_zan@yahoo.com>
2025-01-15 00:07:31 -0800

    Removed unused code

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,14 +1,9 @@
 import { Outlet } from "react-router-dom"
-import Header from "../components/Header"
-import Breadcrumbs from "../components/Breadcrumbs"
+
 
 const RootLayout = () => {
   return <>
-    <Header />
-    <Breadcrumbs />
-    <main className="p-6">
       <Outlet />
-    </main>
   </>
 }
 
